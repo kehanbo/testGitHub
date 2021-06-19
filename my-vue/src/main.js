b339e7ce50760e40abe0382fd167db80a5d18d03
kejinbo <479842342@qq.com>
2021-06-19 23:55:49 +0800

    commit console

--- my-vue/src/main.js
+++ my-vue/src/main.js
@@ -1,11 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import ViewUI from 'view-design';
-import 'view-design/dist/styles/iview.css';
+import Vue from "vue";
+import App from "./App.vue";
+import ViewUI from "view-design";
+import "view-design/dist/styles/iview.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 Vue.use(ViewUI);
 
+console.log("鸡精");
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
