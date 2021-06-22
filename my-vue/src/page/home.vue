5428254dc8c07af11101b09bbfb7dceb2a6520f2
kehanbo <875225214@qq.com>
2021-06-23 00:05:56 +0800

    修改首页

---
+++ my-vue/src/page/home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="main-content">{{name}}</div>
+</template>
+
+<script>
+export default {
+    name: 'home',
+    data() {
+        return {
+            name: '我的家',
+        }
+    },
+    created() {},
+    mounted() {},
+    methods: {
+
+    },
+}
+
+</script>
+
+<style scoped>
+.main-content {
+    height: 100%;
+    width: 100%;
+    padding:20px;
+}
+</style>
