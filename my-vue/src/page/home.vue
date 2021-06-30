16b87edc0f5b5d0c60abf45586b11f90ba290073
kehanbo <875225214@qq.com>
2021-06-30 23:45:45 +0800

    文本编辑器

--- my-vue/src/page/home.vue
+++ my-vue/src/page/home.vue
@@ -1,19 +1,37 @@
 <template>
-  <div class="main-content">{{name}}</div>
+  <div class="main-content">
+       <div class="editor">
+            <div ref="toolbar" class="toolbar">
+            </div>
+            <div ref="editor" class="text">
+            </div>
+        </div>
+  </div>
 </template>
 
 <script>
+import E from 'wangeditor'
+
 export default {
     name: 'home',
     data() {
         return {
             name: '我的家',
+            editor: null,
+            info_: null
         }
     },
-    created() {},
-    mounted() {},
+    created() {
+    },
+    mounted() {
+        this.seteditor()
+        this.editor.txt.html(this.value)
+    },
     methods: {
-
+         seteditor() {
+            this.editor = new E(this.$refs.toolbar, this.$refs.editor) 
+            this.editor.create();
+        },
     },
 }
 
